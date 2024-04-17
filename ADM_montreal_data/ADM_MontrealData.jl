371e3d40fe7dd027a1bf01a8338db28a31c43bf5
konoDioDA253 <ebguessi@gmail.com>
2024-04-16 21:32:58 -0400

    latest update, small changes, updated figures for ADM and ADM MTL

--- ADM_montreal_data/ADM_MontrealData.jl
+++ ADM_montreal_data/ADM_MontrealData.jl
@@ -12,6 +12,8 @@ using Pkg
 # Pkg.add("JSOSolvers")
 # Pkg.add("SolverBenchmark")
 # Pkg.add("Plots")
+Pkg.add("Percival")
+
 
 using LinearAlgebra
 using ADNLPModels
@@ -25,7 +27,7 @@ using CSV
 using Dates
 using SolverBenchmark
 using Plots
-
+using Percival
 
 #**********************************************************************************************************************
 # Function to evaluate the values in a given line in matrix A of the model
@@ -116,7 +118,7 @@ end
 #**********************************************************************************************************************
 # Déclaration des fonctions des problèmes ADNLP et ADNLS
 F(x) = A*x-b        # residual function
-f(x) = sum(abs.(A*x-b))         # Fonction
+f(x) = sum((A*x-b))         # Fonction
 x0 = [0.1; 0.1; 0.1; 0.1; 0.1; 0.1] # first guess
 
 # Modèle ADNLS et résolution ipopt
@@ -171,12 +173,12 @@ print("Performances ipopt\n")
 resultP_ipopt = [stats_p.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; stats_p.elapsed_time]
 print(resultP_ipopt)
 
-print("\nSolution lbfgs\n")
-statsP_lbfgs = lbfgs(nlpp_lsquare)
-print(statsP_lbfgs.solution)
-print("\nPeformances lbfgs\n")
-resultP_lbfgs = [statsP_lbfgs.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_lbfgs.elapsed_time]
-print(resultP_lbfgs)
+# print("\nSolution lbfgs\n")
+# statsP_lbfgs = lbfgs(nlpp_lsquare)
+# print(statsP_lbfgs.solution)
+# print("\nPeformances lbfgs\n")
+# resultP_lbfgs = [statsP_lbfgs.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_lbfgs.elapsed_time]
+# print(resultP_lbfgs)
 
 print("\nSolution tron\n")
 statsP_tron = tron(nlpp_lsquare)
@@ -185,20 +187,28 @@ print("\nPeformances tron\n")
 resultP_tron = [statsP_tron.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_tron.elapsed_time]
 print(resultP_tron)
 
-print("\nSolution trunk\n")
-statsP_trunk = trunk(nlpp_lsquare)
-print(statsP_trunk.solution)
-print("\nPeformances trunk\n")
-resultP_trunk = [statsP_trunk.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_trunk.elapsed_time]
-print(resultP_trunk)
+print("\nSolution Percival\n")
+statsP_percival = percival(nlpp_lsquare)
+print(statsP_percival.solution)
+print("\nPeformances Percival\n")
+resultP_percival = [statsP_percival.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_percival.elapsed_time]
+print(resultP_percival)
 
-print("\nSolution R2\n")
-statsP_R2 = R2(nlpp_lsquare)
-print(statsP_R2.solution)
-print("\nPeformances R2\n")
-resultP_R2 = [statsP_R2.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_R2.elapsed_time]
-print(resultP_R2)
-print(statsP_R2.status)
+# print("\nSolution trunk\n")
+# statsP_trunk = trunk(nlpp_lsquare)
+# print(statsP_trunk.solution)
+# print("\nPeformances trunk\n")
+# resultP_trunk = [statsP_trunk.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_trunk.elapsed_time]
+# print(resultP_trunk)
+
+# print("\nSolution R2\n")
+# statsP_R2 = R2(nlpp_lsquare)
+# print(statsP_R2.solution)
+# print("\nPeformances R2\n")
+# resultP_R2 = [statsP_R2.iter ; nlpp_lsquare.counters.neval_obj ; nlpp_lsquare.counters.neval_grad ; nlpp_lsquare.counters.neval_hess ; statsP_R2.elapsed_time]
+# print(resultP_R2)
+# print(statsP_R2.status)
+
 
 #**********************************************************************************************************************
 # Résultats du modèle NLS avec d'autres solveurs et printing des performances
@@ -214,13 +224,21 @@ print("\nPerformances tron\n")
 resultS_tron = [statsS_tron.iter ; nlps_lsquare.counters.neval_obj ; nlps_lsquare.counters.neval_grad ; nlps_lsquare.counters.neval_hess ; statsS_tron.elapsed_time]
 print(resultS_tron)
 
-print("\nSolution trunk\n")
-statsS_trunk = trunk(nlps_lsquare)
-print(statsS_trunk.solution)
-print("\nPerformances trunk\n")
-resultS_trunk = [statsS_trunk.iter ; nlps_lsquare.counters.neval_obj ; nlps_lsquare.counters.neval_grad ; nlps_lsquare.counters.neval_hess ; statsS_trunk.elapsed_time]
-print(resultS_trunk)
-print(statsP_R2.status)
+print("\nSolution percival\n")
+statsS_percival = percival(nlps_lsquare)
+print(statsS_percival.solution)
+print("\nPerformances percival\n")
+resultS_percival = [statsS_percival.iter ; nlps_lsquare.counters.neval_obj ; nlps_lsquare.counters.neval_grad ; nlps_lsquare.counters.neval_hess ; statsS_percival.elapsed_time]
+print(resultS_percival)
+
+# print("\nSolution trunk\n")
+# statsS_trunk = trunk(nlps_lsquare)
+# print(statsS_trunk.solution)
+# print("\nPerformances trunk\n")
+# resultS_trunk = [statsS_trunk.iter ; nlps_lsquare.counters.neval_obj ; nlps_lsquare.counters.neval_grad ; nlps_lsquare.counters.neval_hess ; statsS_trunk.elapsed_time]
+# print(resultS_trunk)
+# print(statsP_R2.status)
+
 
 
 
@@ -240,18 +258,18 @@ end
 
 # Déclaration des fonctions des problèmes ADNLP et ADNLS
 F(x) = A*x-b[1:m]        # residual function
-f(x) = sum(abs.(A*x-b[1:m]))         # Fonction
+f(x) = sum((A*x-b[1:m]))         # Fonction
 x0 = [0.1; 0.1; 0.1; 0.1; 0.1; 0.1] # first guess
 
-# Modèle ADNLS et résolution lbfgs
+# Modèle ADNLS et résolution ipopt
 nlps_red = ADNLSModel(F, x0, m)
-stats_sred = lbfgs(nlps_red)
+stats_sred = ipopt(nlps_red)
 print(stats_sred.solution)
 
-# Modèle ADNLP et résolution lbfgs
+# Modèle ADNLP et résolution ipopt
 nlpp_red = ADNLPModel(f, x0; constraints=Dict("C1" => λ -> abs(A * λ - b) <= A * λ - b)) # with constraint
 # nlpp_red = ADNLPModel(f, x0) # without constraint
-stats_pred = lbfgs(nlpp_red)
+stats_pred = ipopt(nlpp_red)
 print(stats_pred.solution)
 
 # Représentation graphique des données de température vs les courbes obtenues
